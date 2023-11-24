6eeff0f36a6f0ca4875e959faa048c381db84875
Marina Andressa <admarin73@gmail.com>
2023-11-24 01:32:30 -0300

    comecei tela duvida frequente

---
+++ Telas/DuvidaFrequente/duvida.js
@@ -0,0 +1,32 @@
+const accordion_item = document.querySelectorAll(".accordion_item");
+
+accordion_item.forEach((item) => {
+    const accordion_header_item = item.querySelectorAll(".accordion_header");
+
+    accordion_header_item.addEventListener("click", () => {
+        const accordion_content_item = item.querySelector(".accordion_content");
+
+        const content_actived = document.querySelector(".active");
+
+        VerifyActive(item, accordion_content_item, content_actived);
+    });
+});
+
+function VerifyActive(item, content, content_actived) {
+    const icon_item = item.querySelector(".icon");
+
+    const icons = document.querySelectorAll(".icon");
+
+    icons.forEach((item) => (item.innerHTML = "+"));
+
+    if (content_actived) {
+        content_actived.style.height = 0;
+        content_actived.classList.remove("active");
+    }
+
+    if (content !== content_actived) {
+        icon_item.innerHTML = "-";
+        content.classList.add("active");
+        content.style.height = content.scrollHeigth + 10 + "px";
+    }
+}
